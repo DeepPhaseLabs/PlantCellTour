0a103b07c8ddeaa0445337b2a7ab021b8509436b
nvatan22 <nvatan@Naazanenes-MacBook-Pro-2.local>
2015-07-29 16:51:06 -0400

    back to original spline

---
+++ Assets/Choose.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Choose : MonoBehaviour {
+
+	public int someValue;
+
+	public void ChooseSubway(ref int i, Vector3[] nextPos){
+		if (someValue > 100)
+			i = 1; 
+		else i = 0;
+	
+}
+}
