487056a817f307ad22b2f610a5148dc41fd80d01
DhruvRamanujan <dhruv0122@gmail.com>
2015-07-31 13:16:39 -0400

    Fixed Organelle Arrival Script

---
+++ Assets/OraganelleReachedScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OraganelleReachedScript : MonoBehaviour {
+	public GameObject Organelle;
+	public GameObject OrganelleText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log ("Entered");
+		OrganelleText.SetActive (true);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
