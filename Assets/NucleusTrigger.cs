15bd8b50cc284d2597dcde103183cd8b1abdc4a4
DhruvRamanujan <dhruv0122@gmail.com>
2015-07-31 12:10:20 -0400

    Changed trigger and PlayerCapsule physics.

---
+++ Assets/NucleusTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NucleusTrigger : MonoBehaviour {
+	public GameObject Nucleus;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Debug.Log ("Entered");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
