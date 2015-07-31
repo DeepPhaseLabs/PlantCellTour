d32903b8f448452902883a3e8d69eceb8625ca56
Sophia <sophia1747@gmail.com>
2015-07-31 15:56:50 -0400

    Added audio to oraganellereachedscript

--- Assets/OraganelleReachedScript.cs
+++ Assets/OraganelleReachedScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class OraganelleReachedScript : MonoBehaviour {
 	public GameObject Organelle;
 	public GameObject OrganelleText;
-
+    AudioSource sfx;
 	// Use this for initialization
 	void Start () {
 		OrganelleText.SetActive (false);
@@ -12,7 +12,10 @@ public class OraganelleReachedScript : MonoBehaviour {
 
 	void OnTriggerEnter (Collider other) {
 		Debug.Log ("Entered");
-		OrganelleText.SetActive (true);
+        sfx= Organelle.GetComponent<AudioSource>();
+        sfx.clip = Organelle.GetComponent<AudioSource>().clip;
+        sfx.Play();
+        OrganelleText.SetActive(true);
 	}
 
 	// Update is called once per frame
