79ec4f44cefd909929b833edb9e7bfcce84943b6
Sophia <sophia1747@gmail.com>
2015-08-01 23:01:23 -0400

    audio 1st half, delays, slowed speed

--- Assets/OraganelleReachedScript.cs
+++ Assets/OraganelleReachedScript.cs
@@ -4,17 +4,24 @@ using System.Collections;
 public class OraganelleReachedScript : MonoBehaviour {
 	public GameObject Organelle;
 	public GameObject OrganelleText;
+    bool soundplayed;
     AudioSource sfx;
 	// Use this for initialization
 	void Start () {
 		OrganelleText.SetActive (false);
+        soundplayed = false;
 	}
 
 	void OnTriggerEnter (Collider other) {
 		Debug.Log ("Entered");
         sfx= Organelle.GetComponent<AudioSource>();
         sfx.clip = Organelle.GetComponent<AudioSource>().clip;
-        sfx.Play();
+        if (!soundplayed)
+        {
+            sfx.Play();
+            soundplayed = true;
+        }
+
         OrganelleText.SetActive(true);
 	}
 
