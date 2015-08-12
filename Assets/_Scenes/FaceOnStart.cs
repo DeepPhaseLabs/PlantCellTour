290bc9a290e65254497b5fdf9dc736dddf7cd0f0
Sophia <sophia1747@gmail.com>
2015-08-12 14:26:01 -0400

    more gazepointer stuff. camera rotation issues. Turned off camerapath on start, to revert, go to CameraPath to Nucleus and check "play on start" under CameraPath Animator

--- Assets/_Scenes/FaceOnStart.cs
+++ Assets/_Scenes/FaceOnStart.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+[RequireComponent(typeof(Camera))]
+[RequireComponent(typeof(Transform))]
 
 public class FaceOnStart : MonoBehaviour {
-
-	// Use this for initialization
+    Transform button;
+    Camera cam;
+    // Use this for initialization
 	void Start () {
-	
+	cam.transform.LookAt(button, Vector3.up);
 	}
 	
 	// Update is called once per frame
