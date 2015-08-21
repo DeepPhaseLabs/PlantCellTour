99045f5f6ba85f64706e6c305ce44dfe825feb39
Sophia <sophia1747@gmail.com>
2015-08-21 13:36:40 -0400

    commit

---
+++ Assets/_Scenes/LoadLevelOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelOnTrigger : MonoBehaviour
+{
+
+    public string levelName;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(levelName);
+        }
+    }
+}
