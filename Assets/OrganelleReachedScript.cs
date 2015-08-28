cbce1fc4648f2c1d6fd5faaf559c663eb5e722b2
Sophia <sophia1747@gmail.com>
2015-08-28 14:26:08 -0400

    started fade script

--- Assets/OrganelleReachedScript.cs
+++ Assets/OrganelleReachedScript.cs
@@ -3,30 +3,83 @@ using System.Collections;
 
 public class OrganelleReachedScript : MonoBehaviour {
 	public GameObject Organelle;
-//	public GameObject OrganelleText;
-    bool soundplayed;
+    public GameObject prevOrg;
+    private OrganelleReachedScript a;
+  //  public bool soundplayed;
+float sfxvolume=1.0f;
     AudioSource sfx;
-	// Use this for initialization
 	void Start () {
-	//	OrganelleText.SetActive (false);
-        soundplayed = false;
-	}
-
-	void OnTriggerEnter (Collider other) {
-		Debug.Log (other.gameObject.tag);
-        sfx= Organelle.GetComponent<AudioSource>();
-        sfx.clip = Organelle.GetComponent<AudioSource>().clip;
-        if (!soundplayed)
+        if (prevOrg != null)
         {
-            sfx.Play();
-            soundplayed = true;
+            a = prevOrg.GetComponent<OrganelleReachedScript>();
         }
 
-    //    OrganelleText.SetActive(true);
+	}
+    void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        sfx = Organelle.GetComponent<AudioSource>();
+        sfx.clip = Organelle.GetComponent<AudioSource>().clip;
+        if(a==null)
+        {
+            if(prevOrg.GetComponent<AudioSource>().isPlaying)
+            {
+                fadeOut();
+            }
+            sfx.Play();
+        }
+        else if (a.isAudio())//!soundplayed)
+        {
+            a.fadeOut();
+            sfx.Play();
+        }
+        else
+        {
+
+            sfx.Play();
+            //  soundplayed = true;
+        }
+    }
+      /*  else
+        {
+            a.fadeOut();
+              if (audio1Volume <= 0.1) {
+         if(track2Playing == false)
+         {
+           track2Playing = true;
+           audio.clip = track2;
+           audio.Play();
+         }
+         
+         fadeIn();
+     }
+ }
+        }*/
+    void fadeOut()
+    {
+        if (sfxvolume > 0.1)
+        {
+            sfxvolume -= (float)(0.5 * Time.deltaTime);
+            sfx.volume = sfxvolume;
+        }
+      //  return sfx.volume;
+
+    }
+    bool isAudio()
+    {
+        return sfx.isPlaying;
+    }
 	}
 
 	// Update is called once per frame
-	void Update () {
 	
-	}
-}
+
+  /*  bool getSoundPlayed()
+    {
+        return soundplayed;
+    }*/
+
