8eb86293f4938945bd69d1d99de1b4bb04e66058
Sophia <sophia1747@gmail.com>
2015-08-31 14:54:14 -0400

    smaller reticle, path signs on, button sprites

--- Assets/OrganelleReachedScript.cs
+++ Assets/OrganelleReachedScript.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class OrganelleReachedScript : MonoBehaviour {
 	public GameObject Organelle;
-    public GameObject prevOrg;
-    private OrganelleReachedScript a;
-  //  public bool soundplayed;
-float sfxvolume=1.0f;
+    //public GameObject prevOrg;
+    //private OrganelleReachedScript a;
+     bool soundplayed=false;
+//float sfxvolume=1.0f;
     AudioSource sfx;
 	void Start () {
-        if (prevOrg != null)
-        {
-            a = prevOrg.GetComponent<OrganelleReachedScript>();
-        }
+        //if (prevOrg != null)
+        //{
+        //    a = prevOrg.GetComponent<OrganelleReachedScript>();
+        //}
 
 	}
     void Update () {
@@ -24,55 +24,38 @@ float sfxvolume=1.0f;
         Debug.Log(other.gameObject.tag);
         sfx = Organelle.GetComponent<AudioSource>();
         sfx.clip = Organelle.GetComponent<AudioSource>().clip;
-        if(a==null)
-        {
-            if(prevOrg.GetComponent<AudioSource>().isPlaying)
-            {
-                fadeOut();
-            }
-            sfx.Play();
-        }
-        else if (a.isAudio())//!soundplayed)
-        {
-            a.fadeOut();
-            sfx.Play();
-        }
-        else
+        //if(a==null)
+        //{
+        //    if(prevOrg.GetComponent<AudioSource>().isPlaying)
+        //    {
+        //        fadeOut();
+        //    }
+        //    sfx.Play();
+        //}
+        //else if (a.isAudio())
+        //{
+        //    a.fadeOut();
+        //    sfx.Play();
+        //}
+        //else
+        if(!soundplayed)
         {
 
             sfx.Play();
-            //  soundplayed = true;
         }
     }
-      /*  else
-        {
-            a.fadeOut();
-              if (audio1Volume <= 0.1) {
-         if(track2Playing == false)
-         {
-           track2Playing = true;
-           audio.clip = track2;
-           audio.Play();
-         }
-         
-         fadeIn();
-     }
- }
-        }*/
-    void fadeOut()
-    {
-        if (sfxvolume > 0.1)
-        {
-            sfxvolume -= (float)(0.5 * Time.deltaTime);
-            sfx.volume = sfxvolume;
-        }
-      //  return sfx.volume;
-
-    }
-    bool isAudio()
-    {
-        return sfx.isPlaying;
-    }
+    //void fadeOut()
+    //{
+    //    if (sfxvolume > 0.1)
+    //    {
+    //        sfxvolume -= (float)(0.5 * Time.deltaTime);
+    //        sfx.volume = sfxvolume;
+    //    }
+    //}
+    //bool isAudio()
+    //{
+    //    return sfx.isPlaying;
+    //}
 	}
 
 	// Update is called once per frame
